c4e4ab4983a3a9517bc92b74780e6996304d6b4e
Sadcato <2921735369@qq.com>
2024-11-21 17:08:16 +0800

    learn more 👀

--- src/goblog/go.mod
+++ src/goblog/go.mod
@@ -1,3 +1,11 @@
 module goblog
 
-go 1.21.4
+go 1.22.0
+
+toolchain go1.22.9
+
+require (
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/tools v0.27.0 // indirect
+)
--- src/goblog/main.go
+++ src/goblog/main.go
@@ -6,14 +6,15 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>此博客是用以记录编程笔记，如您有反馈或建议，请联系</h1> "+
-			"</h2>href=\"mailto:summer@example.com\">summer@example.com</h2>")
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"https:baidu.com\">baidu.com</a>")
 	} else {
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我。</p>")
+			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
 
