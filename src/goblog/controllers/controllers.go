f02d6743777238be33e894186bff5b49d1783c4f
Sadcato <2921735369@qq.com>
2024-11-22 21:48:55 +0800

    learn more 👀

--- src/goblog/controllers/controllers.go
+++ src/goblog/controllers/controllers.go
@@ -27,7 +27,7 @@ func ArticlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
 	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
 	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
-	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值: %v <br>", r.PostFormValue("test"))
 }
 
 func ArticlesCreateHandler(w http.ResponseWriter, r *http.Request) {
